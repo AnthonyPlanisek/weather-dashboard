9c056d6f9386b92122d2ee5d28f5ed791843fbb2
Plan49 <anthonyplanisek1@gmail.com>
2021-05-04 14:14:13 -0400

    added the layout

---
+++ script.js
@@ -0,0 +1,34 @@
+var options = "api.openweathermap.org/data/2.5/weather?q=raleigh&appid=bfdb8d3b749b5006a535cd83f463f01f"
+
+
+var city = $("#city")
+
+/*jQuery.ajaxPrefilter(function(options) {
+    if (options.crossDomain && jQuery.support.cors) {
+        options.url = 'https://cors-anywhere.herokuapp.com/' + options.url;
+    }
+});
+
+console.log("y",options)
+*/
+
+
+$("#save").click(function weather(event){
+    event.preventDefault()
+    console.log("I", city.val())
+    console.log("L", 
+    "api.openweathermap.org/data/2.5/weather?q=" + city.val() + "&appid=bfdb8d3b749b5006a535cd83f463f01f")
+
+    var link = "api.openweathermap.org/data/2.5/weather?q=" + city.val() + "&appid=bfdb8d3b749b5006a535cd83f463f01f"
+
+    $.ajax({
+        url: link,
+        method: "GET",
+    }).then(function (response){
+        console.log('AJAX Response \n-------------')
+        console.log(response)
+        console.log("t", response.results.length)
+
+
+    })   
+})
